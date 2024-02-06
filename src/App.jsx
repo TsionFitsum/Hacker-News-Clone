1a9651d32f91662b62b64981f57b9b9d4e0b5b98
TsionFitsum <tsifitsum20@gmail.com>
2024-02-06 17:13:33 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,16 +31,21 @@ return (
   {stories ? 
   <div className='grid gap-2 grid-cols-4 grid-rows-4 text-sm'>{
   stories.map(story => (
-  <article key={story.id} className='flex items-start gap-1 bg-blue-200 rounded-md pt-2'>
+  <article key={story.id} className='flex items-start gap-1 bg-orange-400 rounded-md pt-2'>
+    <div className='text-center'>
     <span>{story.score}</span>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
   <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
 </svg>
-
-    <a href={story.url} target= "_blank" rel="noreferrer" >{story.title}
+</div>
+<div>
+    <a href={story.url} target= "_blank" rel="noreferrer" className='underline font-bold'>
+      {story.title}
     </a>
-    {" "} by {story.by}
+    {" "} 
+    <div className='text-xs mt-1'>by {story.by} </div>
+    </div>
     </article>)) }</div> : (<div><h2>Loading...</h2></div>)}
   </>
 )
