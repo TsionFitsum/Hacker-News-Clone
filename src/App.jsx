04b9b1a6002f9b76d2151b0f730a91085afc6e01
TsionFitsum <tsifitsum20@gmail.com>
2024-02-09 17:34:16 +0300

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 function App() {
  const [stories,setStories] = useState(null);
+ const [comments,setComments] = useState(null);
  useEffect (() => {
   async function fetchData(){
     const getTopStories= await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
@@ -15,8 +16,16 @@ function App() {
     const getStoryData = await fetch(` https://hacker-news.firebaseio.com/v0/item/${storyId}.json`);
     return await getStoryData.json();
 
-
    });
+
+   const commentData = topTwentyStories.map(async (commentId) => {
+    const getComment = await fetch(` https://hacker-news.firebaseio.com/v0/item/${commentId}.json`);
+    return await getComment.json();
+    
+   });
+
+   const allComments = await Promise.all(commentData);
+   setComments(allComments);
 
    const allStories = await Promise.all(storyData);
    setStories(allStories);
@@ -28,23 +37,25 @@ function App() {
 return (
   <>
   <h1 className="text-3xl font-bold pb-3">Hacker News</h1>
-  {stories ? 
+  {stories && comments ? 
   <div className='grid gap-2 grid-cols-4 grid-rows-4 text-sm'>{
-  stories.map(story => (
+  stories && comments.map(story  => (
   <article key={story.id} className='flex items-start gap-1 bg-orange-400 rounded-md pt-2'>
-    <div className='text-center'>
-    <span>{story.score}</span>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
-  <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
-</svg>
-</div>
+   
 <div>
     <a href={story.url} target= "_blank" rel="noreferrer" className='underline font-bold'>
       {story.title}
     </a>
     {" "} 
-    <div className='text-xs mt-1'>by {story.by} </div>
+    <div className='text-xs mt-0'>by {story.by} </div>
+    <div className="flex items-center">
+      
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" 
+      className="ml-1 mt-1 mb-2 w-4 h-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+</svg>
+<div className='text-left'>{story.score}</div>
+    </div>
     </div>
     </article>)) }</div> : (<div><h2>Loading...</h2></div>)}
   </>
